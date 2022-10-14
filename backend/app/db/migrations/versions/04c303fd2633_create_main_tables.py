da7f58a65147730095f1032a299ee1f855c48758
Oleh <okutryk@gmail.com>
2022-10-14 08:57:03 -0400

    resolved conflict

---
+++ backend/app/db/migrations/versions/04c303fd2633_create_main_tables.py
@@ -0,0 +1,21 @@
+"""create_main_tables
+Revision ID: 04c303fd2633
+Revises: 
+Create Date: 2022-10-14 07:54:42.317486
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic
+revision = '04c303fd2633'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
